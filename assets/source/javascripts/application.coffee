ed28730e8739d261552c7ad3bf05dfbb8d6fef26
Magnus Engström <magnus.engstrom@mittmedia.se>
2012-06-26 16:33:46 +0200

    add interface for blog highlight

--- assets/source/javascripts/application.coffee
+++ assets/source/javascripts/application.coffee
@@ -1,1 +1,5 @@
-alert "Hej hopp!"
+@unfair_highlight = 
+	fill_position: (position, blog_url_name, id) ->
+		jQuery("#site_sitemeta_blog_highlight_" + position).val(id)
+		jQuery("#highlight-position-#{position}").html(blog_url_name)
+		return
--- views/unfair_highlight/index.php
+++ views/unfair_highlight/index.php
@@ -1,24 +1,31 @@
-<?php global $site; global $blogs; global $highlight_blogs; ?>
-
+<?php global $site; global $blogs; global $highlight_blogs; 
+      echo "<script type='text/javascript' src='" . WP_PLUGIN_URL . "/unfair_highlight/assets/build/javascripts/application.js'></script>";
+?>
 <div class="wrap">
   <div id="icon-options-general" class="icon32"><br></div>
   <h2><?php _e( 'Unfair Highlight Settings' ); ?></h2>
-    <table class="wp-list-table widefat" cellspacing="0">
-      <thead>
-      <tr>
-        <th scope="col" id="description" class="manage-column column-description" style="">Position 1</th>
-        <th scope="col" id="description" class="manage-column column-description" style="">Position 2</th>
-        <th scope="col" id="description" class="manage-column column-description" style="">Position 3</th>
-      </tr>
-      </thead>
-      <tbody id="the-list">
-        <tr><td id="highlight-position-1"><?php echo $highlight_blogs[0]->path ?></td>
-        <td id="highlight-position-2"><?php echo $highlight_blogs[1]->path ?></td>
-        <td id="highlight-position-3"><?php echo $highlight_blogs[2]->path ?></td></tr>
-      </body>
-    </table>
-    <br />
-
+  <p>Välj vilka bloggar som ska visas bland utvalda bloggar</p>
+    <form action='/wp-admin/network/settings.php?page=unfair_highlight_settings' method='post'>
+      <input type="hidden" name='site[sitemeta][blog_highlight_1][meta_value]' id='site_sitemeta_blog_highlight_1' value="<?php echo $highlight_blogs[0]->blog_id ?>" />
+      <input type="hidden" name='site[sitemeta][blog_highlight_2][meta_value]' id='site_sitemeta_blog_highlight_2' value="<?php echo $highlight_blogs[1]->blog_id ?>" />
+      <input type="hidden" name='site[sitemeta][blog_highlight_3][meta_value]' id='site_sitemeta_blog_highlight_3' value="<?php echo $highlight_blogs[2]->blog_id ?>" />
+    
+      <table class="wp-list-table widefat" cellspacing="0">
+        <thead>
+        <tr>
+          <th scope="col" id="description" class="manage-column column-description" style="width: 33%">Position 1</th>
+          <th scope="col" id="description" class="manage-column column-description" style="width: 33%">Position 2</th>
+          <th scope="col" id="description" class="manage-column column-description" style="width: 33%">Position 3</th>
+        </tr>
+        </thead>
+        <tbody id="the-list">
+          <tr><td id="highlight-position-1"><?php echo $highlight_blogs[0]->path ?></td>
+          <td id="highlight-position-2"><?php echo $highlight_blogs[1]->path ?></td>
+          <td id="highlight-position-3"><?php echo $highlight_blogs[2]->path ?></td>
+        </body>
+      </table>
+      <p class="submit"><input type="submit" name="submit" id="submit" class="button-primary" value="Spara ändringar"></p>
+    </form>
     <table class="wp-list-table widefat" cellspacing="0">
     <thead>
     <tr>
@@ -30,7 +37,13 @@
   <?php
       foreach ( $blogs as $blog ) {
         //echo $blog->path;
-        echo "<tr><td>" . $blog->path . "</td><td><button>Position 1</button></td><td><button>Position 2</button></td><td><button>Position 3</button></td></tr>";
+        ?>
+<tr>
+<td><?php echo $blog->path ?></td><td><button onclick="window.unfair_highlight.fill_position(1, '<?php echo $blog->path ?>', <?php echo $blog->blog_id ?>)" class='button_fill_position'>Position 1</button></td>
+<td><button onclick="window.unfair_highlight.fill_position(2, '<?php echo $blog->path ?>', <?php echo $blog->blog_id ?>)" class='button_fill_position'>Position 2</button></td>
+<td><button onclick="window.unfair_highlight.fill_position(2, '<?php echo $blog->path ?>', <?php echo $blog->blog_id ?>)" class='button_fill_position'>Position 3</button></td>
+</tr>
+        <?php
       }
   /*$content = array(
     array(
